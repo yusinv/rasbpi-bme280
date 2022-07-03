26751c0b012cd404be3b8f8120c81672a544e283
yusin <yusinv@gmail.com>
2022-07-03 23:13:44 +0300

    added auto discovery

--- rasbpi_bme280/mqtt_publish.py
+++ rasbpi_bme280/mqtt_publish.py
@@ -1,51 +1,71 @@
+import json
+import sys
 import time
+import yaml
 
 import paho.mqtt.publish as publish
 
 import rasbpi_bme280.bme280 as bme280
 
-mqtt_broker_host = "nas"
-
-# sub_topic = "sensor/instructions"    # receive messages on this topic
-
-pub_topic = "home/hall/"  # send messages to this topic
-
-
 # when connecting to mqtt do this;
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code " + str(rc))
-    # client.subscribe(sub_topic)
+# def on_connect(client, userdata, flags, rc):
+#     print("Connected with result code " + str(rc))
 
 
-# when receiving a mqtt message do this;
-# def on_message(client, userdata, msg):
-#     message = str(msg.payload)
-#     print(msg.topic+" "+message)
-#     display_sensehat(message)
-
-# def publish_mqtt(sensor_data):
-#     mqttc = mqtt.Client("python_pub")
-#     mqttc.connect(Broker, 1883)
-#     mqttc.publish(pub_topic, sensor_data)
-# mqttc.loop(2) //timeout = 2s
-
-# def on_publish(mosq, obj, mid):
-#     print("mid: " + str(mid))
+config = {}
 
 
-# client = mqtt.Client()
-# client.on_connect = on_connect
-# client.on_message = on_message
-# client.connect(mqtt_broker_host, 1883, 60)
+def load_conf_file(config_file: str) -> None:
+    with open(config_file, "r") as f:
+        cfg = yaml.safe_load(f)
+
+    global config
+    config = cfg['config']
+
 
 def main():
+    if len(sys.argv) > 1:
+        config_path = sys.argv[1]
+    else:
+        config_path = '/etc/bme280_mqtt_conf.yaml'
+
+    load_conf_file(config_path)
+
+    if 'discovery' in config:
+        discovery_conf = config['discovery']
+        temperature_sensor_conf = {
+            'name': '{} Temperature'.format(discovery_conf.get("name", "BME 280")),
+            'device_class': 'temperature',
+            'state_topic': config.get('temperature_topic', 'temp'),
+            'unit_of_measurement': '°C'
+        }
+        pressure_sensor_conf = {
+            'name': '{} Pressure'.format(discovery_conf.get("name", "BME 280")),
+            'device_class': 'pressure',
+            'state_topic': config.get('pressure_topic', 'pressure'),
+            'unit_of_measurement': 'mmHg'
+        }
+        humidity_sensor_conf = {
+            'name': '{} Humidity'.format(discovery_conf.get("name", "BME 280")),
+            'device_class': 'humidity',
+            'state_topic': config.get('humidity_topic', 'humidity'),
+            'unit_of_measurement': '%'
+        }
+        msgs = [('{}/sensor/{}_t/config'.format(discovery_conf['prefix'], discovery_conf['id']),
+                 json.dumps(temperature_sensor_conf), 0, False),
+                ('{}/sensor/{}_p/config'.format(discovery_conf['prefix'], discovery_conf['id']),
+                 json.dumps(pressure_sensor_conf), 0, False),
+                ('{}/sensor/{}_h/config'.format(discovery_conf['prefix'], discovery_conf['id']),
+                 json.dumps(humidity_sensor_conf), 0, False)]
+        publish.multiple(msgs, hostname=config['mqtt_broker_host'])
+
     while True:
         temperature, pressure, humidity = bme280.readBME280All()
-        msgs = [(pub_topic + "temperature", str(temperature), 0, False),
-                (pub_topic + "pressure", str(pressure*0.75006375541921), 0, False),
-                (pub_topic + "humidity", str(humidity), 0, False)]
+        msgs = [(config.get('temperature_topic', 'temp'), '{:.2f}'.format(temperature), 0, False),
+                (config.get('pressure_topic', 'pressure'), '{:.2f}'.format(pressure * 0.75006375541921), 0, False),
+                (config.get('humidity_topic', 'humidity'), '{:.2f}'.format(humidity), 0, False)]
 
-        publish.multiple(msgs, hostname=mqtt_broker_host)
+        publish.multiple(msgs, hostname=config['mqtt_broker_host'])
         time.sleep(1 * 60)
 
 
--- setup.py
+++ setup.py
@@ -2,17 +2,18 @@ from setuptools import setup
 
 setup(
     name='rasbpi_bme280',
-    version='',
+    version='0.0.1',
     url='',
     license='',
     author='valentin',
     author_email='',
-    description='', install_requires=['paho-mqtt'],
+    description='', install_requires=['paho-mqtt', 'pyyaml'],
     packages=['rasbpi_bme280'],
     entry_points={
         'console_scripts': [
             'bme280_mqtt=rasbpi_bme280.mqtt_publish:main',
         ],
     },
-    data_files=[('/etc/systemd/system/', ['extra/bme280_mqtt.service'])],
+    data_files=[('/etc/systemd/system/', ['extra/bme280_mqtt.service']),
+                ('/etc', ['extra/bme280_mqtt_conf.yaml'])],
 )
