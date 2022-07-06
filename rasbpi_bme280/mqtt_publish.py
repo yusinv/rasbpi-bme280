6e6e495e38bbd84c7b7f499d1bd6f7570fefe6f6
yusin <yusinv@gmail.com>
2022-07-06 18:42:52 +0300

    added retain flag

--- rasbpi_bme280/mqtt_publish.py
+++ rasbpi_bme280/mqtt_publish.py
@@ -34,36 +34,36 @@ def main():
     if 'discovery' in config:
         discovery_conf = config['discovery']
         temperature_sensor_conf = {
-            'name': '{} Temperature'.format(discovery_conf.get("name", "BME 280")),
+            'name': f'{discovery_conf.get("name", "BME 280")} Temperature',
             'device_class': 'temperature',
             'state_topic': config.get('temperature_topic', 'temp'),
             'unit_of_measurement': '°C'
         }
         pressure_sensor_conf = {
-            'name': '{} Pressure'.format(discovery_conf.get("name", "BME 280")),
+            'name': f'{discovery_conf.get("name", "BME 280")} Pressure',
             'device_class': 'pressure',
             'state_topic': config.get('pressure_topic', 'pressure'),
             'unit_of_measurement': 'mmHg'
         }
         humidity_sensor_conf = {
-            'name': '{} Humidity'.format(discovery_conf.get("name", "BME 280")),
+            'name': f'{discovery_conf.get("name", "BME 280")} Humidity',
             'device_class': 'humidity',
             'state_topic': config.get('humidity_topic', 'humidity'),
             'unit_of_measurement': '%'
         }
-        msgs = [('{}/sensor/{}_t/config'.format(discovery_conf['prefix'], discovery_conf['id']),
-                 json.dumps(temperature_sensor_conf), 0, False),
-                ('{}/sensor/{}_p/config'.format(discovery_conf['prefix'], discovery_conf['id']),
-                 json.dumps(pressure_sensor_conf), 0, False),
-                ('{}/sensor/{}_h/config'.format(discovery_conf['prefix'], discovery_conf['id']),
-                 json.dumps(humidity_sensor_conf), 0, False)]
+        msgs = [(f'{discovery_conf["prefix"]}/sensor/{discovery_conf["id"]}_t/config',
+                 json.dumps(temperature_sensor_conf), 0, True),
+                (f'{discovery_conf["prefix"]}/sensor/{discovery_conf["id"]}_p/config',
+                 json.dumps(pressure_sensor_conf), 0, True),
+                (f'{discovery_conf["prefix"]}/sensor/{discovery_conf["id"]}_h/config',
+                 json.dumps(humidity_sensor_conf), 0, True)]
         publish.multiple(msgs, hostname=config['mqtt_broker_host'])
 
     while True:
         temperature, pressure, humidity = bme280.readBME280All()
-        msgs = [(config.get('temperature_topic', 'temp'), '{:.2f}'.format(temperature), 0, False),
-                (config.get('pressure_topic', 'pressure'), '{:.2f}'.format(pressure * 0.75006375541921), 0, False),
-                (config.get('humidity_topic', 'humidity'), '{:.2f}'.format(humidity), 0, False)]
+        msgs = [(config.get('temperature_topic', 'temp'), f'{temperature:.2f}', 0, False),
+                (config.get('pressure_topic', 'pressure'), f'{pressure * 0.75006375541921:.2f}', 0, False),
+                (config.get('humidity_topic', 'humidity'), f'{humidity:.2f}', 0, False)]
 
         publish.multiple(msgs, hostname=config['mqtt_broker_host'])
         time.sleep(1 * 60)
